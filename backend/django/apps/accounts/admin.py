c915ae9da1ef6bb425f47eb47c570eaf1f08d5da
slavpetroff <slavpetroff@gmail.com>
2017-07-11 22:33:07 +0300

    Create the admin panel settings for accounts

--- backend/django/apps/accounts/admin.py
+++ backend/django/apps/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from polymorphic.admin import PolymorphicParentModelAdmin, \
+        PolymorphicChildModelAdmin, PolymorphicChildModelFilter
+from .models import AbstractAccount
 
-# Register your models here.
+
+class BaseAccountAdmin(PolymorphicChildModelAdmin):
+    """ Base admin class for all child models """
+    base_model = AbstractAccount
+
+
+@admin.register(AbstractAccount)
+class ParentAccountAdmin(PolymorphicParentModelAdmin):
+    """ The parent model admin """
+    base_model = AbstractAccount
+    child_models = (AbstractAccount,)
+    list_filter = (PolymorphicChildModelFilter,)
