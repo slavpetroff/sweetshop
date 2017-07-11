03bc8f7de193975db6a471d1edb21f75fcc619e6
slavpetroff <slavpetroff@gmail.com>
2017-07-11 22:32:34 +0300

    Create the ViewSet of the Accounts

--- backend/django/apps/accounts/views.py
+++ backend/django/apps/accounts/views.py
@@ -1,5 +1,9 @@
-from rest_framework import viewsets
+from core.base_viewset import BaseViewSet
+from .models import AbstractAccount
+
+from .serializers import WholeAccountSerializer
 
 
-class AccountViewSet(viewsets.ModelViewSet):
-    pass
+class AccountViewSet(BaseViewSet):
+    queryset = AbstractAccount.objects.all()
+    serializer_class = WholeAccountSerializer
