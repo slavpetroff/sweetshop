3e4c389f590b3f9209a8a2686557889895b81f9e
slavpetroff <slavpetroff@gmail.com>
2017-07-12 14:41:49 +0300

    WIP: Create initial tests for the `BaseAccount`

--- backend/django/apps/accounts/tests.py
+++ backend/django/apps/accounts/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+import factory
+from .models import BaseAccount
 
-# Create your tests here.
+
+class UserFactory(factory.Factory):
+
+    class Meta:
+        model = BaseAccount
+
+    first_name = 'John'
+    last_name = 'Doe'
+    email = '{}.{}@email.com'.format(first_name, last_name)
+    password = 'passjohn1'
+
+
+class CreateUserTest(APITestCase):
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.client.login(email=self.user.email, password=self.user.password)
+
+    def test_can_create_user(self):
+        response = self.client.get(
+                reverse('_accounts:account-list'),)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
